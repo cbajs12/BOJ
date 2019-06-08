1e6ac6c98fe1336dd48db146199f8ebe7c4e216f
전지성 <cbajs20@gmail.com>
2019-06-08 21:14:58 +0900

    leetcode pause

--- leetcode/leetcode_str_4.c
+++ leetcode/leetcode_str_4.c
@@ -1,38 +1,43 @@
-
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* findDiagonalOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize){
-    int row = matrixSize / *matrixColSize;
-    int col = *matrixColSize;
-    int *returnSize = matrixSize;
-    int returnArry = (int *)malloc(*returnSize*(sizeof(int)));
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int* findDiagonalOrder(int** matrix, int matrixRowSize, int matrixColSize, int* returnSize){
+    int row = matrixRowSize;
+    int col = matrixColSize;
+    *returnSize = matrixRowSize * matrixColSize;
+    int* returnArry = (int *)malloc(*returnSize*(sizeof(int)));
     int cnt = 0;
-    int least = 0;
+    int start = 0;
 
-    if(row > col){
-        least = col;
-    }else{
-        least = row;
+    int chunk = col + row - 1;
+
+
+    for(int i = 0; i <= chunk; ++i){
+        if(i % 2 == 0){
+            start = i;
+
+            if(start > matrixRowSize - 1){
+                start = matrixRowSize - 1;
+            }
+
+            for(int j = start; (j >= 0) && (i-j < matrixColSize); j--){
+                returnArry[cnt++] = matrix[j][i-j];
+                //cnt++;
+            }
+        }else{
+            start = i;
+
+            if(start > matrixColSize - 1){
+                start = matrixColSize - 1;
+            }
+
+            for(int j = start; (j >= 0) && (i-j < matrixRowSize); j--){
+                returnArry[cnt++] = matrix[i-j][j];
+                //cnt++;
+            }
+        }
     }
-
-//    for(int i =0; i < row; ++i){
-//        for(int j= 0; j<col; ++j){
-//            if(i == 0 && j==0) {
-//                returnArry[0] = matrix[i][j];
-//                cnt++;
-//            }else if(i == row-1 && j == col-1 ){
-//                returnArry[*returnSize-1] = matrix[i][j];
-//                cnt++;
-//            }else{
-//                if(i+j)
-//            }
-//        }
-//    }
-
-    while(cnt < matrixSize){
-
-    }
+    return returnArry;
 }
 
 
--- leetcode/main.c
+++ leetcode/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include "leetcode_str_2.c"
+#include "leetcode_str_4.c"
 int main() {
     int numsize = 2;
-    int nums[2] = {1, 2};
+    int nums[3][3] = [[1,2,3],[4,5,6],[7,8,9]];
 
-    dominantIndex(nums, numsize);
+    //findDiagonalOrder(nums, 3, 3, int* returnSize){
     return 0;
 }
